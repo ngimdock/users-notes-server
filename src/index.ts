570539cab2ec505d18187d9f772bfd92b2829e06
ngimdock <ngimdockzemfack@gmail.com>
2023-04-08 13:20:32 +0100

    Retrieving users

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+
 const port = Number(process.env.API_PORT) || 4000;
+
+import { schema } from "./schema";
+
+const yoga = createYoga({
+  schema,
+});
+
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.info(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
---
+++ src/schema.ts
@@ -0,0 +1,6 @@
+import { builder } from "./builder";
+
+import "./models/user";
+import "./models/message";
+
+export const schema = builder.toSchema({});
